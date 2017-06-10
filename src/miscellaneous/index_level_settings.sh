01b7d0a1c81a99a045c0c6efd43438789a9ffb6a
MorelessCSS <rf@appon.net>
2017-06-10 08:13:59 +0200

    initial

--- src/miscellaneous/index_level_settings.sh
+++ src/miscellaneous/index_level_settings.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -XPUT -k -u "elasticsearch:to4get" 'http://elasticsearch:9200/_all/_settings?preserve_existing=true' -d '{"index.auto_expand_replicas" : "0-all"}'
+curl -XPUT -k -u "elasticsearch:to4get" 'http://localhost:9200/_all/_settings?preserve_existing=true' -d '{"index.auto_expand_replicas" : "0-all"}'
--- src/miscellaneous/wait_until_started.sh
+++ src/miscellaneous/wait_until_started.sh
@@ -3,7 +3,7 @@ RET=1
 
 while [[ RET -ne 0 ]]; do
     echo "Stalling for Elasticsearch..."
-    curl -XGET http://elasticsearch:9200/" >/dev/null 2>&1
+    curl -XGET http://localhost:9200/" >/dev/null 2>&1
     RET=$?
     sleep 30
 done
